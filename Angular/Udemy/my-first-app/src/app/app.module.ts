a74931456c6c5120210db1dae096084c72f5df3e
hitesh-realbotz <hitesh.patil@realbotz.com>
2023-11-06 16:00:46 +0530

    Angular Tutorial - Routing Section

--- Angular/Udemy/my-first-app/src/app/app.module.ts
+++ Angular/Udemy/my-first-app/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+// import { ServerComponent } from './server/server.component';
+// import { ServersComponent } from './servers/servers.component';
 import { CockpitComponent } from './cockpit/cockpit.component';
 import { ServerElementComponent } from './server-element/server-element.component';
 import { ViewEmulComponent } from './view-emul/view-emul.component';
@@ -19,6 +19,37 @@ import { AccountComponent } from './services-and-dependency/services/account/acc
 import { NewAccountComponent } from './services-and-dependency/services/new-account/new-account.component';
 import { AccountsService } from './services-and-dependency/services/accounts.service';
 import { LoggingService } from './services-and-dependency/services/logging.service';
+import { RoutingComponent } from './routing/routing.component';
+
+
+// comment earlier import of ServiceComponent, ServerComponent
+import { HomeComponent } from './routing/home/home.component';
+import { UsersComponent } from './routing/users/users.component';
+import { ServersComponent } from './routing/servers/servers.component';
+import { EditServerComponent } from './routing/servers/edit-server/edit-server.component';
+import { ServerComponent } from './routing/servers/server/server.component';
+import { UserComponent } from './routing/users/user/user.component';
+import { ServersService } from './routing/servers/servers.service';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'users', component: UsersComponent, children:[
+    {
+      path: ':id/:name', component: UserComponent
+    }
+  ] },
+  // { path: 'users', component: UsersComponent},
+  // { path: 'users/:id/:name', component: UserComponent },
+  {
+    path: 'servers', component: ServersComponent, children: [
+      { path: ':id', component: ServerComponent },
+      { path: ':id/edit', component: EditServerComponent },
+    ]
+  },
+  // { path:'servers/:id', component: ServerComponent},
+  // { path:'servers/:id/edit', component: EditServerComponent},
+];
 
 @NgModule({
   declarations: [
@@ -36,13 +67,19 @@ import { LoggingService } from './services-and-dependency/services/logging.servi
     UnlessDirective,
     ServicesComponent,
     AccountComponent,
-    NewAccountComponent
+    NewAccountComponent,
+    RoutingComponent,
+    HomeComponent,
+    UsersComponent,
+    EditServerComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [AccountsService,LoggingService],  // for application scope/level instantiation
+  providers: [AccountsService, LoggingService, ServersService],  // for application scope/level instantiation
   bootstrap: [AppComponent]
 })
 export class AppModule { }
