649336f720ba780000673c6f059f0e1b269f9e0e
hitesh-realbotz <hitesh.patil@realbotz.com>
2023-11-22 17:51:18 +0530

    Angular Tutorial - Authentication section complete

--- Angular/Udemy/prj-start/src/app/app.component.ts
+++ Angular/Udemy/prj-start/src/app/app.component.ts
@@ -1,19 +1,24 @@
-import { Component, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
 import { filter, map, of } from 'rxjs';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'prj-start';
-
-
+  
   // Not required in-case of Routing
   loadedFeature = 'recipe';
   onNavigate(feature: string) {
     this.loadedFeature = feature;
   }
 
+  constructor(private authService: AuthService){ }
+  
+  ngOnInit() {
+   this.authService.autoLogin();
+  }
 }
--- Angular/Udemy/prj-start/src/app/auth/auth.component.ts
+++ Angular/Udemy/prj-start/src/app/auth/auth.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { AuthResponseData, AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -9,8 +11,10 @@ import { AuthService } from './auth.service';
 })
 export class AuthComponent {
   isLoginMode = true;
+  isLoading = false;
+  error: string = null;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -23,18 +27,56 @@ export class AuthComponent {
     }
     const email = form.value.email;
     const password = form.value.password;
+    let authObs: Observable<AuthResponseData>;
+    this.isLoading = true;
 
     if (this.isLoginMode) {
+      authObs = this.authService.login(email, password);
 
     } else {
+      authObs = this.authService.signup(email, password);
 
-      this.authService.signup(email, password).subscribe(resData => {
-        console.log(resData);
-      }, error => {
-        console.log(error);
+      // this.authService.signup(email, password).subscribe(
+      //   resData => {
+      //     console.log(resData);
+      //     this.isLoading = false;
+      //   },
+      //   // // error resp display from component itself
+      //   // errorRes => {
+      //   //   console.log(errorRes);
+      //   //   switch (errorRes.error.error.message) {
+      //   //     case "EMAIL_EXISTS": 
+      //   //       this.error = 'This email-Id already registered!'
+      //   //       break;
+      //   //     default:
+      //   //       break;
+      //   //   }
+      //   //   // this.error = 'An Error Occured!'
+      //   //   this.isLoading = false;
+      //   // }
 
-      });
+      //   // error resp display from auth.service
+      //   errorMessage => {
+      //     console.log(errorMessage);
+      //     this.error = errorMessage;
+      //     this.isLoading = false;
+      //   }
+      // );
     }
+
+    authObs.subscribe(
+      resData => {
+        console.log(resData);
+        this.isLoading = false;
+        this.router.navigate(['/recipes']);
+      },
+      errorMessage => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    );
+
     form.reset();
   }
 }
