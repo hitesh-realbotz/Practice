c2c1eb52b68193767065220ad16e5b244aeba3d8
hitesh-realbotz <hitesh.patil@realbotz.com>
2023-12-27 15:30:45 +0530

    C# Tutorial - Console, Operators & Conditional statements Sections

--- C#/Udemy/PrimitiveDataTypes/04.Boolean/Program.cs
+++ C#/Udemy/PrimitiveDataTypes/04.Boolean/Program.cs
@@ -8,10 +8,11 @@ namespace _04.Boolean
 {
     internal class Program
     {
-        
+
 
         static void Main(string[] args)
         {
+            /*
             float fNo = 673.14f;
             double dNumber = 1234.56;
             Console.WriteLine(fNo);
@@ -23,11 +24,12 @@ namespace _04.Boolean
             //char sign = '\u00D6';
             char sign = (char)65;
             Console.WriteLine(sign);
-            //Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
             Console.WriteLine(Console.OutputEncoding);
 
             string userName = "admin";
-            
+
             Console.WriteLine(userName[4]);
 
             //int[] numbers = { 5, 6, 7, 8, 9, 1, 2, 3 };
@@ -37,13 +39,73 @@ namespace _04.Boolean
             Console.WriteLine(separator);
             int intNo = 456;
             Console.WriteLine(intNo + intNo + userName + intNo + intNo);
-            char a = 'a';
-            char b = 'b';
-            char c = 'c';
-            string v1 = "First " + a + " Second " + b + " Third " + c;
-            string v2 = string.Format("First {0} Second {1} Third {2} for {0} {1} {2} ",a,b,c);
+            char charA = 'a';
+            char charB = 'b';
+            char charC = 'c';
+            string v1 = "First " + charA + " Second " + charB + " Third " + charC;
+            string v2 = string.Format("First {0} Second {1} Third {2} for {0} {1} {2} ", charA, charB, charC);
+            string v3 = $"First {charA} Second {charB} Third {charC} for {charA} {charB} {charC}";
             Console.WriteLine(v1);
             Console.WriteLine(v2);
+            Console.WriteLine(v3);
+
+            int number = 10;
+            string num = "5";
+            //string num = null;
+            Console.WriteLine(Convert.ToInt32(num));    //0 incase of num = null
+            Console.WriteLine(int.Parse(num));          //Exception incase of num = null
+            Console.WriteLine(separator);
+            string v4 = "C:\\Folder1\\File1";
+            v4 = "\"C:\\Folder1\\File1\"";
+            v4 = @"""C:\Folder1\File1""";
+            Console.WriteLine(v4);
+
+            Console.WriteLine(separator);
+            string fruit = "Strawberry";
+            string juice = "juice";
+            float dollerSign = 1000.56f;
+            Console.WriteLine("{0, 15}", fruit);
+            Console.WriteLine("{0, 15}", juice);
+            Console.WriteLine("{0, 15:C2}", dollerSign);
+            Console.WriteLine("{0:C2}", dollerSign);
+            Console.WriteLine("{0, 15:N7}", dollerSign);
+            Console.WriteLine("{0:N7}", dollerSign);
+            Console.WriteLine($"{fruit, 15}");
+            Console.WriteLine($"{juice, 15}");
+            Console.WriteLine($"{dollerSign, 15:C2}");
+            Console.WriteLine($"{dollerSign:C2}");
+            Console.WriteLine($"{dollerSign, 15:N7}");
+            Console.WriteLine($"{dollerSign:N7}");
+            
+
+            ConsoleKeyInfo key =  Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine(key.KeyChar);
+            Console.WriteLine(key.Modifiers);
+            string mod = key.Modifiers.ToString();
+            string[] modArray = mod.Split(',');
+            Console.WriteLine(modArray[0]);
+            */
+
+
+            Console.CursorSize = 100;
+
+            Console.WriteLine(Console.WindowHeight +" "+ Console.WindowWidth);
+            Console.WriteLine(Console.LargestWindowHeight +" "+ Console.LargestWindowWidth);
+
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                case 0:
+                    Console.WriteLine($"Number is 0 ");
+                    break;
+                case 1:
+                    Console.WriteLine($"Number is 1");
+                    break;
+                default: Console.WriteLine("Default");
+                    break;
+            }
+
         }
     }
 }
