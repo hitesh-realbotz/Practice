0271b9b45913fa3600bff40be9358f4cb77a3657
hitesh-realbotz <hitesh.patil@realbotz.com>
2024-01-02 14:47:39 +0530

    C# Tutorial - Class, Fields, Properties, Constructors, Access Modifiers Sections

---
+++ C#/Udemy/IntroToOops/IntroToOops/Program.cs
@@ -0,0 +1,46 @@
+﻿using IntroToOops.ClassTemplates.Characters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToOops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Warrior goodGuy = new Warrior();
+            Console.WriteLine($"Count : {Warrior.IdCounter}");
+
+            Warrior badGuy = new Warrior("John", 11, 70);
+            Console.WriteLine($"Count : {Warrior.IdCounter}");
+            Warrior thirdWarrior = new Warrior();
+            Console.WriteLine($"Count : {Warrior.IdCounter}");
+
+
+            Console.WriteLine(goodGuy.Name);
+
+            Console.WriteLine(badGuy.Name);
+
+            //Console.WriteLine(goodGuy.GetName());
+            //Console.WriteLine(badGuy.GetName());
+            //try
+            //{
+            //    goodGuy.Level = 500;
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("In Catch");
+            //    Console.WriteLine("Exception Handled!");
+            //    Console.WriteLine(ex.Message);
+            //}
+            //goodGuy.Level = 50;
+            //Console.WriteLine(goodGuy.Level);
+            Console.WriteLine(thirdWarrior.Level);
+
+
+        }
+    }
+}
---
+++ C#/Udemy/MagicDestroyers - Part 8/MagicDestroyers/Equipment/Weapons/Blunt/Staff (2).cs
@@ -0,0 +1,62 @@
+﻿using System;
+using MagicDestroyers.Equipment.Interfaces;
+
+namespace MagicDestroyers.Equipment.Weapons.Blunt
+{
+    public class Staff : Blunt, IBuff
+    {
+        private const int DEFAULT_WEAPONSTAMINA = 10;
+        private const int DEFAULT_WEAPONDAMAGE = 11;
+        private const int DEFAULT_STAMINA = 1;
+
+        public Staff()
+            : this(DEFAULT_WEAPONDAMAGE)
+        {
+
+        }
+        public Staff(int damage)
+            :this(damage, 10)
+        {
+
+        }
+
+        public Staff(float stamina)
+            : this(10, DEFAULT_WEAPONSTAMINA)
+        {
+
+        }
+
+        public Staff(int damage = DEFAULT_WEAPONDAMAGE, int stamina = DEFAULT_WEAPONSTAMINA)
+        {
+            this.DamagePoints = damage;
+        }
+
+
+        //public Staff()
+        //    : this(DEFAULT_DAMAGE_POINTS)
+        //{
+        //}
+
+        //public Staff(int armorPoints)
+        //{
+        //    this.DamagePoints = armorPoints;
+        //}
+
+        public override void SpecialAbility()
+        {
+            this.Buff();
+        }
+
+        public void Buff()
+        {
+            this.Empower();
+            // More buff abilities...
+            // ...
+        }
+
+        public void Empower()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
