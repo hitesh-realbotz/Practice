be04f5b031b9ac71d6965c0187c6b949f3a0a790
hitesh-realbotz <hitesh.patil@realbotz.com>
2024-01-25 18:16:54 +0530

    ASP.NET Core WebAPI Tutorial - Sorting, Pagination, Authentication & Authorization

--- ASP.NET Core/Udemy/NZWalks/NZWalksAPI/Repositories/IRegionRepository.cs
+++ ASP.NET Core/Udemy/NZWalks/NZWalksAPI/Repositories/IRegionRepository.cs
@@ -6,7 +6,6 @@ namespace NZWalksAPI.Repositories
     public interface IRegionRepository
     {
         Task<List<Region>> GetAllAsync();
-        //Task<List<Region>> GetAllAsync(string? filterOn = null, string? filterQuery = null);
         Task<Region?> GetByIdAsync(Guid id);
 
         Task<Region> CreateAsync(Region region);
