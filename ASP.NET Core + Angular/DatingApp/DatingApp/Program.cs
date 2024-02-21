793676739610382285a42374ae39f40f3482fb53
hitesh-realbotz <hitesh.patil@realbotz.com>
2024-02-21 18:30:42 +0530

    ASP.NET Core + Angular Tutorial : Extending API, Building UI & Update section

--- ASP.NET Core + Angular/DatingApp/DatingApp/Program.cs
+++ ASP.NET Core + Angular/DatingApp/DatingApp/Program.cs
@@ -1,5 +1,7 @@
+using DatingApp.Data;
 using DatingApp.Extensions;
 using DatingApp.Middlewares;
+using Microsoft.EntityFrameworkCore;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -34,5 +36,21 @@ app.UseAuthorization();
 
 
 app.MapControllers();
+
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
+
 
 app.Run();
