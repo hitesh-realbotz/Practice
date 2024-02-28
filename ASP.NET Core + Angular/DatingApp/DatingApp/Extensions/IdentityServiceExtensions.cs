f15020688673b1620172acadcc50d890f48c3a6b
hitesh-realbotz <hitesh.patil@realbotz.com>
2024-02-28 18:18:46 +0530

    ASP.NET Core + Angular Tutorial : Identity & Role management section

--- ASP.NET Core + Angular/DatingApp/DatingApp/Extensions/IdentityServiceExtensions.cs
+++ ASP.NET Core + Angular/DatingApp/DatingApp/Extensions/IdentityServiceExtensions.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
@@ -8,6 +11,15 @@ namespace DatingApp.Extensions
     {
         public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
         {
+            services.AddIdentityCore<AppUser>(opt =>
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+            })
+            .AddRoles<AppRole>()
+            .AddRoleManager<RoleManager<AppRole>>()
+            .AddEntityFrameworkStores<DataContext>();
+
+
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
             {
                 ValidateIssuer = false,
@@ -15,6 +27,12 @@ namespace DatingApp.Extensions
                 ValidateLifetime = true,
                 ValidateIssuerSigningKey = true,
                 IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]))
+            });
+
+            services.AddAuthorization(opt =>
+            {
+                opt.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+                opt.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
             });
 
             return services;
--- ASP.NET Core + Angular/DatingApp/DatingApp/Services/TokenService.cs
+++ ASP.NET Core + Angular/DatingApp/DatingApp/Services/TokenService.cs
@@ -1,5 +1,6 @@
 ﻿using DatingApp.Entities;
 using DatingApp.Interfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -10,20 +11,27 @@ namespace DatingApp.Services
     public class TokenService : ITokenService
     {
         private readonly IConfiguration _config;
+        private readonly UserManager<AppUser> _userManager;
         private readonly SymmetricSecurityKey key;
 
-        public TokenService(IConfiguration config)
+        public TokenService(IConfiguration config, UserManager<AppUser> userManager)
         {
             key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
             _config = config;
+            _userManager = userManager;
         }
-        public string CreateToken(AppUser user)
+        public async Task<string> CreateToken(AppUser user)
         {
             var claims = new List<Claim>()
             {
                  new Claim(JwtRegisteredClaimNames.NameId, user.Id.ToString()),
                 new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName),
             };
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
+
             var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
             var tokenDescriptor = new SecurityTokenDescriptor
             {
@@ -37,5 +45,37 @@ namespace DatingApp.Services
 
             return tokenHandler.WriteToken(token);
         }
+
+        ////Without ASP.NET Identity
+        //private readonly IConfiguration _config;
+        //private readonly SymmetricSecurityKey key;
+
+        //public TokenService(IConfiguration config)
+        //{
+        //    key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        //    _config = config;
+        //}
+        //public string CreateToken(AppUser user)
+        //{
+        //    var claims = new List<Claim>()
+        //    {
+        //         new Claim(JwtRegisteredClaimNames.NameId, user.Id.ToString()),
+        //        new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName),
+        //    };
+        //    var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+        //    var tokenDescriptor = new SecurityTokenDescriptor
+        //    {
+        //        Subject = new ClaimsIdentity(claims),
+        //        Expires = DateTime.Now.AddDays(7),
+        //        SigningCredentials = cred
+        //    };
+
+        //    var tokenHandler = new JwtSecurityTokenHandler();
+        //    var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        //    return tokenHandler.WriteToken(token);
+        //}
+
     }
+
 }
