c05bdcd9afbe71e7f485b98f69539c5621fcb69c
hitesh-realbotz <hitesh.patil@realbotz.com>
2024-02-29 18:09:58 +0530

    ASP.NET Core + Angular Tutorial : SignalR section

--- ASP.NET Core + Angular/DatingApp/DatingApp/Data/DataContext.cs
+++ ASP.NET Core + Angular/DatingApp/DatingApp/Data/DataContext.cs
@@ -5,47 +5,6 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.Data
 {
-    //public class DataContext : DbContext
-    //{
-    //    public DataContext(DbContextOptions options) : base(options)
-    //    {
-    //    }
-
-    //    public DbSet<UserLike> Likes { get; set; }
-    //    public DbSet<Message> Messages { get; set; }
-
-    //    protected override void OnModelCreating(ModelBuilder builder)
-    //    {
-    //        base.OnModelCreating(builder);
-
-    //        builder.Entity<UserLike>()
-    //            .HasKey(k => new { k.SourceUserId, k.TargetUserId });
-
-    //        builder.Entity<UserLike>()
-    //            .HasOne(s => s.SourceUser)
-    //            .WithMany(l => l.LikedUsers)
-    //            .HasForeignKey(s => s.SourceUserId)
-    //            .OnDelete(DeleteBehavior.Cascade);
-
-    //        builder.Entity<UserLike>()
-    //            .HasOne(s => s.TargetUser)
-    //            .WithMany(l => l.LikedByUsers)
-    //            .HasForeignKey(s => s.TargetUserId)
-    //            .OnDelete(DeleteBehavior.Cascade);
-
-    //        builder.Entity<Message>()
-    //           .HasOne(u => u.Recipient)
-    //           .WithMany(m => m.MessagesReceived)
-    //           .OnDelete(DeleteBehavior.Restrict);
-
-    //        builder.Entity<Message>()
-    //            .HasOne(u => u.Sender)
-    //            .WithMany(m => m.MessagesSent)
-    //            .OnDelete(DeleteBehavior.Restrict);
-    //    }
-    //}
-
-    //With ASP.NET Identity
     public class DataContext : IdentityDbContext<AppUser, AppRole, int,
                                                         IdentityUserClaim<int>, AppUserRole, IdentityUserLogin<int>,
                                                         IdentityRoleClaim<int>, IdentityUserToken<int>>
@@ -56,6 +15,8 @@ namespace DatingApp.Data
 
         public DbSet<UserLike> Likes { get; set; }
         public DbSet<Message> Messages { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Connection> Connections { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -98,6 +59,49 @@ namespace DatingApp.Data
                 .WithMany(m => m.MessagesSent)
                 .OnDelete(DeleteBehavior.Restrict);
         }
+
+
+
+        //Without ASP.NET Identity
+        //public class DataContext : DbContext
+        //{
+        //    public DataContext(DbContextOptions options) : base(options)
+        //    {
+        //    }
+
+        //    public DbSet<UserLike> Likes { get; set; }
+        //    public DbSet<Message> Messages { get; set; }
+
+        //    protected override void OnModelCreating(ModelBuilder builder)
+        //    {
+        //        base.OnModelCreating(builder);
+
+        //        builder.Entity<UserLike>()
+        //            .HasKey(k => new { k.SourceUserId, k.TargetUserId });
+
+        //        builder.Entity<UserLike>()
+        //            .HasOne(s => s.SourceUser)
+        //            .WithMany(l => l.LikedUsers)
+        //            .HasForeignKey(s => s.SourceUserId)
+        //            .OnDelete(DeleteBehavior.Cascade);
+
+        //        builder.Entity<UserLike>()
+        //            .HasOne(s => s.TargetUser)
+        //            .WithMany(l => l.LikedByUsers)
+        //            .HasForeignKey(s => s.TargetUserId)
+        //            .OnDelete(DeleteBehavior.Cascade);
+
+        //        builder.Entity<Message>()
+        //           .HasOne(u => u.Recipient)
+        //           .WithMany(m => m.MessagesReceived)
+        //           .OnDelete(DeleteBehavior.Restrict);
+
+        //        builder.Entity<Message>()
+        //            .HasOne(u => u.Sender)
+        //            .WithMany(m => m.MessagesSent)
+        //            .OnDelete(DeleteBehavior.Restrict);
+        //    }
+        //}
     }
 
 
