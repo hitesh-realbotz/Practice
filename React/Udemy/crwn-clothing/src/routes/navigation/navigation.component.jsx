e12524f7c3dcf0b90d99cca2c0ee2246699129a8
hitesh-realbotz <hitesh.patil@realbotz.com>
2024-04-04 17:58:39 +0530

    React Tutorial : Authentication-Firebase, State Management & Observer section

--- React/Udemy/crwn-clothing/src/routes/navigation/navigation.component.jsx
+++ React/Udemy/crwn-clothing/src/routes/navigation/navigation.component.jsx
@@ -1,10 +1,19 @@
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
+import { UserContext } from '../../contexts/user.context';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
-import './navigation.styles.scss'
+import { signOutUser } from '../../utils/firebase/firebase.utils';
+
+import './navigation.styles.scss';
 
 const Navigation = () => {
 
+    const { currentUser, setCurrentUser } = useContext(UserContext);
+
+    const signOutHandler = async () => {
+        await signOutUser();
+        // setCurrentUser(null);
+      };
 
     return (
         <Fragment>
@@ -16,9 +25,16 @@ const Navigation = () => {
                     <Link className='nav-link' to='/shop'>
                         SHOP
                     </Link>
-                    <Link className='nav-link' to='/sign-in'>
-                        SIGN IN
-                    </Link>
+                    {currentUser ? (
+                        <span className='nav-link' onClick={signOutHandler}>
+                            {' '}
+                            SIGN OUT{' '}
+                        </span>
+                    ) : (
+                        <Link className='nav-link' to='/auth'>
+                            SIGN IN
+                        </Link>
+                    )}
                 </div>
             </div>
             <Outlet />
