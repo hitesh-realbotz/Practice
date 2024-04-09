15fc978adc53d2f7ee78b8e60de14eb55fb3ea85
hitesh-realbotz <hitesh.patil@realbotz.com>
2024-04-09 17:34:34 +0530

    React Tutorial : Redux-Saga section

--- React/Udemy/crwn-clothing/src/App.js
+++ React/Udemy/crwn-clothing/src/App.js
@@ -8,26 +8,15 @@ import Shop from "./routes/shop/shop.component";
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
-import {
-  onAuthStateChangedListener,
-  createUserDocumentFromAuth,
-} from './utils/firebase/firebase.utils';
-import { setCurrentUser } from './store/user/user.action';
+import { checkUserSession } from './store/user/user.action';
 
 
 const App = () => {
 
   const dispatch = useDispatch();
-
+ 
   useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
-      if (user) {
-        createUserDocumentFromAuth(user);
-      }
-      dispatch(setCurrentUser(user));
-    });
-
-    return unsubscribe;
+    dispatch(checkUserSession());
   }, []);
 
   return (
