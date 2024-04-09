32df6b91d33f5628c0bb52ea902e0632eec88e71
hitesh-realbotz <hitesh.patil@realbotz.com>
2024-04-09 15:33:44 +0530

    React Tutorial : Redux-Thunk section

--- React/Udemy/crwn-clothing/src/utils/firebase/firebase.utils.js
+++ React/Udemy/crwn-clothing/src/utils/firebase/firebase.utils.js
@@ -20,22 +20,13 @@ import {
   getDocs,
 } from 'firebase/firestore';
 
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-//   authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-//   projectId: 'crwn-clothing-db-98d4d',
-//   storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-//   messagingSenderId: '626766232035',
-//   appId: '1:626766232035:web:506621582dab103a4d08d6',
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyD3GV59cBUW9WXaIoIVhktylzI1h_AXqZ8",
-  authDomain: "crwn-clothing-db-ceabe.firebaseapp.com",
-  projectId: "crwn-clothing-db-ceabe",
-  storageBucket: "crwn-clothing-db-ceabe.appspot.com",
-  messagingSenderId: "925971308027",
-  appId: "1:925971308027:web:c7a05d1b529d577ccd7f5e"
+  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
+  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
+  projectId: 'crwn-clothing-db-98d4d',
+  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
+  messagingSenderId: '626766232035',
+  appId: '1:626766232035:web:506621582dab103a4d08d6',
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
@@ -76,13 +67,7 @@ export const getCategoriesAndDocuments = async () => {
   const q = query(collectionRef);
 
   const querySnapshot = await getDocs(q);
-  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
-    const { title, items } = docSnapshot.data();
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
-
-  return categoryMap;
+  return querySnapshot.docs.map((docSnapshot) => docSnapshot.data());
 };
 
 export const createUserDocumentFromAuth = async (
