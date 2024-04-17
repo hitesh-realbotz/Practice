0c46c1c0832dbbb548bc4dbe56e6c4d5bb5e4230
hitesh-realbotz <hitesh.patil@realbotz.com>
2024-04-17 11:29:49 +0530

    React Tutorial : PWA section - crwn-clothing-ts

--- React/Udemy/crwn-clothing-ts/src/index.js
+++ React/Udemy/crwn-clothing-ts/src/index.js
@@ -15,6 +15,7 @@ import { store, persistor } from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { stripePromise } from './utils/stripe/stripe.utils';
 import { Elements } from "@stripe/react-stripe-js";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -44,6 +45,8 @@ root.render(
   // </React.StrictMode>
 
 );
+
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
